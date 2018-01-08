182b196af8310e63e76acb879d1354e04fbc07cf
Steven Peniche <stevenmpeniche@gmail.com>
2018-01-08 17:15:25 -0500

    Binary Search Tree: complete

--- 01-data-structures/06-trees/binary_tree/node.rb
+++ 01-data-structures/06-trees/binary_tree/node.rb
@@ -1,9 +1,23 @@
 class Node
   attr_accessor :title
   attr_accessor :rating
+  attr_accessor :parent
   attr_accessor :left
   attr_accessor :right
 
   def initialize(title, rating)
+    @title = title
+    @rating = rating
+    @parent = nil
+    @left = nil
+    @right = nil
+  end
+
+  def >(node)
+    self.rating > node.rating
+  end
+
+  def <(node)
+    self.rating < node.rating
   end
 end
