2f9bcaba3199ed2f78aa4b6ccb7f5317257ffade
Steven Peniche <stevenmpeniche@gmail.com>
2018-01-10 15:50:26 -0500

    Find Kevin Bacon: revised

--- 01-data-structures/find_kevin_bacon.rb
+++ 01-data-structures/find_kevin_bacon.rb
@@ -1,18 +1,21 @@
-films_with_kev = ['Footloose'..]
+class Node
+  attr_accessor :name
+  attr_accessor :film_actor_hash
+  attr_accessor :films # An array of films the actor is in
+end
+
+path_to_kev = []
+visited = []
 
 def find_kevin_bacon(start_node)
-  kev_connections = []
-  for film in films_with_kev
-    if kev_connections.length < 6
-      film_actors = Kevin_Bacon.film_actor_hash[film]
-      for actor in film_actors
-        if actor === start_node.name
-          kev_connections.push film
+  if path_to_kev.length < 6
+    for film in start_node.films
+      if visited.include?(film) === false
+        for actor in start_node.film_actor_hash[film]
+          path_to_kev.push film
+          actor === Kevin_Bacon ? return path_to_kev : find_kevin_bacon(actor)
         end
       end
-    else
-      break
     end
   end
-  kev_connections
 end
